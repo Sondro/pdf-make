bce5056be66ff8b332cb2e0f6bcf588ccf244b14
Sondro <memoiran@hotmail.com>
2018-04-06 02:50:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var pdfmake = require('./build/pdfmake.js');
+var vfs_fonts = require('./build/vfs_fonts.js');
+var bingoCards = require('./examples/bingo-cards.js');
+
+//import { pdfmake } from './build/pdfmake';
+//import { vfs_fonts } from './build/vfs_fonts';
+//import { bingo-cards } from './examples/bingo-cards';
+/*
+function main() {
+
+	console.log('asdfasdf');
+
+	//undefined:
+	console.log(printer);
+	console.log(fonts);
+	pdfMake.createPdf(docDefinition).open();
+}
+
+main();
+*/
